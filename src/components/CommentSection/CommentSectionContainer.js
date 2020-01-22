f43873b3ec3020558b1455ef7481f9b16cd8411f
Ike Otu <ikeotu@msn.com>
2020-01-21 22:42:07 -0800

    comment section container complete

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,11 +6,17 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+  const [userComments] = useState(props.comments);
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+
+      {userComments.map(elem => (
+        <Comment comment={elem} />
+      ))}
       <CommentInput />
+      
     </div>
   );
 };
--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -10,7 +10,7 @@ import "./Posts.css";
 const Post = props => {
   // set up state for the likes
   const [likes, setLikes] = useState(props.post.likes);
-  const moreLikes = () => {setLikes (likes + 1)};
+  const moreLikes = () => setLikes (likes + 1);
 
   return (
     <div className="post-border">
